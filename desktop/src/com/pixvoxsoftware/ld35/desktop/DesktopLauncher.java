952fdf38a12f57722a3a6bbe5bebdb31a9bddf48
PixelIndigo <m4s4n0bu@gmail.com>
2016-04-16 01:28:25 +0300

    Initial commit

---
+++ desktop/src/com/pixvoxsoftware/ld35/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pixvoxsoftware.ld35.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pixvoxsoftware.ld35.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
