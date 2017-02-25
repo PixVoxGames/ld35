9e1fdcb94e08f4f99a410072bf37e896251a494c
pixelindigo <m4s4n0bu@gmail.com>
2017-02-25 17:33:38 +0300

    fixed game crash in wayland environment

--- desktop/src/com/pixvoxsoftware/ld35/desktop/DesktopLauncher.java
+++ desktop/src/com/pixvoxsoftware/ld35/desktop/DesktopLauncher.java
@@ -1,16 +1,14 @@
 package com.pixvoxsoftware.ld35.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.pixvoxsoftware.ld35.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
-		config.title = "The Witch & The Witcher";
-		config.fullscreen = false;
-		new LwjglApplication(new Game(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+                config.setWindowedMode(1280, 720);
+		config.setTitle("The Witch & The Witcher");
+		new Lwjgl3Application(new Game(), config);
 	}
 }
